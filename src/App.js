a58bac6555d86ff6e984e052ad28df0b2c47ba54
duyjack <46467707+duyjack@users.noreply.github.com>
2020-05-15 15:36:11 +0700

    update

--- src/App.js
+++ src/App.js
@@ -2,15 +2,30 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+class TextParagraph extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: 'App.js'
+    }
+  }
+
+  render() {
+    return (
+      <p>
+        Edit <code>{this.state.text}</code> and save to reload.
+      </p>
+    );
+  }
+}
+
 export default class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
+          <TextParagraph />
           <a
             className="App-link"
             href="https://reactjs.org"
